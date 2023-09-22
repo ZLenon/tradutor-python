8397ae0bdb68de2eff68401cf0cf3620807ef98d
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-22 13:47:24 -0300

    Test

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -3,10 +3,10 @@ from src.models.history_model import HistoryModel
 
 
 def test_request_history():
-    json_tests = HistoryModel.list_as_json()
-    tests = json.loads(json_tests)
+    testes_json = HistoryModel.list_as_json()
+    tests = json.loads(testes_json)
 
     assert len(tests) == 2
-    assert tests[0]["text_to_translate"] == "do you like bubble gum?"
+    assert tests[0]["text_to_translate"] == "I drink water in the morning"
     assert tests[0]["translate_from"] == "en"
     assert tests[0]["translate_to"] == "pt"
