85d7a045730ac49c3eeacee2f2a58c725db00dd7
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-22 14:31:57 -0300

    Fix

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,25 +1,24 @@
 import json
 from src.models.history_model import HistoryModel
 
+mock_traducao = [
+    {
+        "text_to_translate": "i drink water in the morning",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+    {
+        "text_to_translate": "I study often",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+]
 
-def test_request_history():
-    historico_json = HistoryModel.list_as_json()
-    historico_string = json.loads(historico_json)
+
+def test_request_history(prepare_base):
+    historico_string = json.loads(HistoryModel.list_as_json())
 
     for x in historico_string:
         x.pop("_id")
-
-    mock_traducao = [
-        {
-            "text_to_translate": "i drink water in the morning",
-            "translate_from": "en",
-            "translate_to": "pt",
-        },
-        {
-            "text_to_translate": "I study often",
-            "translate_from": "en",
-            "translate_to": "pt",
-        },
-    ]
 
     assert historico_string == mock_traducao
