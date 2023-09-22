bb373718e92d913fb73df0d61d4bc8938d758ad5
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-22 11:30:36 -0300

    Feat: refatorando

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -7,29 +7,6 @@ from models.language_model import LanguageModel
 translate_controller = Blueprint("translate_controller", __name__)
 
 
-@translate_controller.route("/reverse", methods=["POST"])
-def reverse():
-    languages = LanguageModel.list_dicts()
-    text_to_translate = request.form.get("text-to-translate")
-    translate_from = request.form.get("translate-from")
-    translate_to = request.form.get("translate-to")
-
-    translate_from, translate_to = translate_to, translate_from
-
-    translator = GoogleTranslator(
-        source="auto", target=translate_from
-    ).translate(text_to_translate)
-
-    return render_template(
-        "index.html",
-        languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translator,
-    )
-
-
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
@@ -56,4 +33,27 @@ def index():
         translated = "Tradução"
     return render_template(
         "index.html", languages=languages, translated=translated
+    )
+
+
+@translate_controller.route("/reverse", methods=["POST"])
+def reverse():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translate_from, translate_to = translate_to, translate_from
+
+    translator = GoogleTranslator(
+        source="auto", target=translate_from
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translator,
     )
