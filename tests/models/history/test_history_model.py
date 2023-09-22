8fe494009338c5000266527593a594678d0597ae
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-22 14:08:21 -0300

    Fix

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -3,25 +3,23 @@ from src.models.history_model import HistoryModel
 
 
 def test_request_history():
-    testes_json = HistoryModel.list_as_json()
-    tests = json.loads(testes_json)
+    historico_json = HistoryModel.list_as_json()
+    historico_string = json.loads(historico_json)
 
-    assert len(tests) == 2
-    assert tests[0]["text_to_translate"] == "I drink water in the morning"
-    assert tests[0]["translate_from"] == "en"
-    assert tests[0]["translate_to"] == "pt"
+    for x in historico_string:
+        x.pop("_id")
 
-    for item in tests:
-        item.pop("_id")
-    assert tests == [
+    mock_traducao = [
         {
-            "text_to_translate": "I have a dog, you has a cat",
+            "text_to_translate": "i drink water in the morning",
             "translate_from": "en",
             "translate_to": "pt",
         },
         {
-            "text_to_translate": "I play the guitar, but i dont play other",
+            "text_to_translate": "I study often",
             "translate_from": "en",
             "translate_to": "pt",
         },
     ]
+
+    assert historico_string == mock_traducao
